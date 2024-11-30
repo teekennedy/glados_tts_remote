7475630118555653ecb0e6664c16207c12d8d2f8
Terrance Kennedy <terrance@missingtoken.net>
2024-11-30 01:13:31 -0700

    Add entry setup and unload functions

--- custom_components/glados_tts_remote/__init__.py
+++ custom_components/glados_tts_remote/__init__.py
@@ -1,1 +1,19 @@
 """Adds GLaDOS TTS Engine"""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload Entry"""
+    all_ok = True
+    for platform in PLATFORMS:
+        if not await hass.config_entries.async_forward_entry_unload(entry, platform):
+            all_ok = False
+
+    return all_ok

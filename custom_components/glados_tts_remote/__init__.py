84dbaba44596fce3d9a6daa41aafaaa90db61c08
Terrance Kennedy <terrance@missingtoken.net>
2025-08-03 13:09:36 -0600

    Start with a clean slate

--- custom_components/glados_tts_remote/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Adds GLaDOS TTS Engine"""
-from .const import DOMAIN
-from .tts import async_setup_entry, async_unload_entry
--- custom_components/glados_tts_remote/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for glados_tts_remote"""
-
-DOMAIN = "glados_tts_remote"
-
-SUPPORTED_LANGUAGES = ["en-US"]
-
-DEFAULT_LANG = "en-US"
-DEFAULT_URL = "http://localhost:8124"
-DEFAULT_TIMEOUT = 5
